874dc82a479a78ba4afeb2f485791f513214aade
Mohammad Atikur Rhaman <mohammadatikur.rhaman@optimizely.com>
2024-11-26 18:58:12 +0600

    Initial commit

---
+++ eTickets/Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext _context)
+        {
+            this._context = _context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+            return View(data);
+        }
+    }
+}
