a37ae3bafb1ed89f7980cb78d5b002bddcbd7962
Mohammad Atikur Rhaman <mohammadatikur.rhaman@optimizely.com>
2024-11-27 16:02:38 +0600

    Add model validation for the Actor model Finalize the code to create an Actor

--- eTickets/Models/Actor.cs
+++ eTickets/Models/Actor.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace eTickets.Models
 {
@@ -8,15 +9,20 @@ namespace eTickets.Models
         public int Id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Pr    ofile Picture is required")]
         public string ProfilePictureURL { get; set; }
 
 		[Display(Name = "Full Name")]
+		[Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
 		public string FullName { get; set; }
 
 		[Display(Name = "Biography")]
+		[Required(ErrorMessage = "Biography is required")]
 		public string Bio { get; set; }
 
         //Relationships
+        [ValidateNever]
         public List<Actor_Movie> Actors_Movies { get; set; }
     }
 }
