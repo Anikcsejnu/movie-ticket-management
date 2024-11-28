7fdc150d9a35840d06d85fc838005be1c2156fd3
Mohammad Atikur Rhaman <mohammadatikur.rhaman@optimizely.com>
2024-11-28 19:23:59 +0600

    Add CRUD operation for the produrecer model

--- eTickets/Controllers/ProducersController.cs
+++ eTickets/Controllers/ProducersController.cs
@@ -1,4 +1,6 @@
 ﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,17 +8,45 @@ namespace eTickets.Controllers
 {
     public class ProducersController : Controller
     {
-        private readonly AppDbContext _context;
+        private readonly IProducersService _service;
 
-        public ProducersController(AppDbContext context)
+        public ProducersController(IProducersService service)
         {
-            this._context = context;
+            this._service = service;
         }
 
         public async Task<IActionResult> Index()
         {
-            var allProducers = await _context.Producers.ToListAsync();
+            var allProducers = await _service.GetAllAsync();
             return View(allProducers);
         }
+
+        // Get: producers/details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var producerDetails = await _service.GetByIdAsync(id);
+            if (producerDetails != null)
+            {
+                return View(producerDetails);
+            }
+
+            return View("NotFound");
+        }
+
+        // Get: producers/create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("ProfilePictureURL, Bio,FullName")] Producer producer)
+        {
+            if (!ModelState.IsValid) return View(producer);
+            await _service.AddAsync(producer);
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 });
 
 builder.Services.AddScoped<IActorsService, ActorsService>();
+builder.Services.AddScoped<IProducersService, ProducersService>();
 
 var app = builder.Build();
 
